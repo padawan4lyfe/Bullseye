f520bdc1f43b03c54a72a67a60e105b9cdd2e0ca
padawan4lyfe <joshua.hawley@me.com>
2018-10-23 18:45:55 -0500

    Labels in place no functionality yet

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -15,11 +15,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func showAlert() {
+        
+       let alert = UIAlertController(title: "Hello, World!", message: "This is my first App", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
     }
-
-
 }
 
